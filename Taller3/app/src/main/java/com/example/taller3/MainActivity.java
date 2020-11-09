cecd938c837ec5e25c2fdd46895277f8086ecbb0
Juan Castañeda <castaneda_juan@javeriana.eduu.co>
2020-11-09 16:38:53 -0500

    Adelanto

--- Taller3/app/src/main/java/com/example/taller3/MainActivity.java
+++ Taller3/app/src/main/java/com/example/taller3/MainActivity.java
@@ -1,14 +1,112 @@
 package com.example.taller3;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText usuarioText, contraseñaText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        this.usuarioText=findViewById(R.id.userText);
+        this.contraseñaText=findViewById(R.id.passwordText);
+        Button login = findViewById(R.id.boton_login);
+        TextView registro = findViewById(R.id.link_registro);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInUser(usuarioText.getText().toString(),contraseñaText.getText().toString());
+            }
+        });
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Registro.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser){
+        if(currentUser!=null){
+            /*Intent intent = new Intent(getBaseContext(), Navigation.class);
+            intent.putExtra("user", currentUser.getEmail());
+            startActivity(intent);*/
+        } else {
+            contraseñaText.setText("");
+            usuarioText.setText("");
+        }
+    }
+
+    private void signInUser(String email, String password) {
+        if (validateForm()) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("LOG", "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                Log.w("Login", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                updateUI(null);
+                            }
+                        }
+                    });
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+        String user = usuarioText.getText().toString();
+        if (TextUtils.isEmpty(user)) {
+            usuarioText.setError("Required.");
+            valid = false;
+        } else {
+            usuarioText.setError(null);
+        }
+        String pass = contraseñaText.getText().toString();
+        if (TextUtils.isEmpty(pass)) {
+            contraseñaText.setError("Required.");
+            valid = false;
+        } else {
+            contraseñaText.setError(null);
+        }
+        return valid;
     }
 }
